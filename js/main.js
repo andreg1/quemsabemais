3b0ff0b3e3d01314dbe36ef8c0b11b72fdbc1533
André Brito <andrebritog1@gmail.com>
2018-07-17 16:17:51 +0100

    17/07

--- js/main.js
+++ js/main.js
@@ -8,45 +8,47 @@ class QuizzEngine {
     constructor() {
         this.loadData();
     }
+
     //update methods
-    saveData(){
+    saveData() {
         $.ajax({
             url: "ajax/save.php",
             data: this.data,
         });
     }
-    updateCategory(target){
-        var $row = target.parents("tr");
+    updateCategory($row) {
         try {
-            var id = $("#tblCategorias tbody tr.edit-row").index($row) + 1;
+            var id = $row.attr("data-Id");
 
-            this.categories[id].name = $row.;
+            this.categories[id].name = $row.find("input[name=categoryName]").val();
             this.saveData();
 
             this.loadCategories();
-            $row.toggle();
-            $row.prev(".view-row").toggle();
         } catch (error) {
             $("#lblCategoryError").text("Ocorreu um erro.");
             console.log(error);
         }
-        
+
 
     }
     //create methods
-    addCategory(name) {
+    createCategory(name) {
         try {
             this.categories[this.nextCategoryID++] = { name };
             this.saveData();
+            this.loadCategories();
         } catch (error) {
             $("#lblCategoryError").text("Ocorreu um erro.");
             console.log(error);
         }
     }
+    createQuizz(){
+        var name = $("#newQuizzForm input[name=quizzName]").val();
+
+    }
     //delete methods
-    deleteCategory(target) {
-        var $row = target.parents("tr");
-        var id = $("#tblCategorias tbody tr").index($row) + 1;
+    deleteCategory($row) {
+        var id = $row.attr("data-Id");
         try {
             delete this.categories[id];
             this.saveData();
@@ -76,20 +78,27 @@ class QuizzEngine {
         this.clearCategories();
 
         $.each(this.categories, (index, value) => {
-            $('#tblCategorias tbody').append(`
-                <tr class="view-row">
-                    <td>${value.name}</td>
+            $('#tblCategories tbody').append(`
+                <tr data-Id="${index}">
+                    <td><label id="lblCategoryName">${value.name}</label></td>
                     <td><button class="editButton">Editar</button></td>
                     <td><button class="deleteButton">Apagar</button></td>
                 </tr>
-                <tr class="edit-row">
-                    <td><input type="text" value="${value.name}"/></td>
-                    <td><button class="saveButton">Gravar</button></td>
-                </tr>`);
+            `);
         });
 
         var categories = Object.keys(this.categories);
         this.nextCategoryID = parseInt(categories[categories.length - 1]) + 1;
+    }
+    loadCategoriesDropdown(){
+        $("select[name=quizzCategory] option").remove();
+
+        //add placeholder
+        $("select[name=quizzCategory]").append(`<option value="-1">Escolha uma Categoria</option>`);
+
+        $.each(this.categories, (index, category) => {
+            $("select[name=quizzCategory]").append(`<option value="${index}">${category.name}</option>`);
+        });
     }
     // loadQuizzes(categoryID) {
     //     this.clearQuizzes();
@@ -114,37 +123,73 @@ class QuizzEngine {
             });
         });
     }
+    //checks
+    categoryExists(categoryID){
+        return this.categories[categoryID] === undefined ? false : true;
+    }
     //general use
-    clearCategories(){
+    clearCategories() {
         $("#tblCategories tbody").empty();
     }
     clearQuizzes() {
         $("#tblQuizzes tbody").empty();
     }
-    editMode(target){
-        var $row = target.parents("tr");
-        $row.toggle();
-        $row.next(".edit-row").toggle();
+    viewMode($row) {
+        $row.html(`
+            <td>${$row.find("input[name=categoryName]").val()}</td>
+            <td><button class="editButton">Editar</button></td>
+            <td><button class="deleteButton">Apagar</button></td>
+        `);
+    }
+    editMode($row) {
+        $row.html(`
+            <td><input type="text" name="categoryName" class="form-control" value="${$row.find("#lblCategoryName").text()}"/></td>
+            <td><button class="saveButton">Gravar</button></td>
+        `);
+    }
+    addQuizzView(target){
+        this.loadCategoriesDropdown(); //load first, then show
+
+        $(target).hide();
+        $('#newQuizzForm').removeClass('d-none');
+    }
+    addQuestionView(target){
+
+        $('#questionList').hide();
+        $('#newQuestionForm').removeClass('d-none');
     }
 }
 var engine;
 $(document).ready(function () {
     engine = new QuizzEngine();
-    $("#tblCategorias").on("click", "button.deleteButton", (event) => {
-        engine.deleteCategory($(event.target));
+    $("#tblCategories").on("click", "button.deleteButton", (event) => {
+        engine.deleteCategory($(event.target).parents("tr"));
     });
-    $("#tblCategorias").on("click", "button.saveButton", (event) => {
-        engine.updateCategory($(event.target));
+    $("#tblCategories").on("click", "button.saveButton", (event) => {
+        engine.updateCategory($(event.target).parents("tr"));
+    });
+    $("#btNewCategory").click(() => {
+        var categoryName = $('input[name=tbCategory]').val();
+        engine.createCategory(categoryName);
+        $('input[name=tbCategory]').val(""); // clear val
     });
 
-    
+
     $("#tblQuizzes").on("click", "button.deleteButton", (event) => {
-        engine.deleteQuizz($(event.target));
+        engine.deleteQuizz($(event.target).parents("tr"));
+    });
+    $("#tblCategories, #tblQuizzes").on("click", "button.editButton", (event) => {
+        engine.editMode($(event.target).parents("tr"));
+    });
+    $("#btNewQuizz").click((event) => {
+        engine.addQuizzView(event.target);
+    });
+    $("#newQuizzForm button[type=submit]").click(() => {
+        engine.createQuizz();
     });
 
-
-    $("#tblCategorias, #tblQuizzes").on("click", "button.editButton", (event) => {
-        engine.editMode($(event.target));
+    $('#addQuestion').click((event)=>{
+        engine.addQuestionView(event.target);
     });
     //engine.loadCategories();
 });
